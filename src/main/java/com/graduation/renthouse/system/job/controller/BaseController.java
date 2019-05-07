6e908e49532dc8fc0f0dd1af2e218fbcdf04a7db
Li <1083460362@qq.com>
2019-05-07 23:48:24 +0800

    add job

---
+++ src/main/java/com/graduation/renthouse/system/job/controller/BaseController.java
@@ -0,0 +1,9 @@
+package com.graduation.renthouse.system.job.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BaseController {
+
+}
---
+++ src/main/java/com/graduation/renthouse/system/job/task/NewTask.java
@@ -0,0 +1,15 @@
+package com.graduation.renthouse.system.job.task;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewTask implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("定时任务");
+    }
+}
